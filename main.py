48efc41e9be46ab2df6dc248c988f76bb77bc466
praveen <praveenkrarumugam@gmail.com>
2025-06-24 06:54:07 +0000

    Test auto deploy trigger

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "🚀 Auto Deployed Flask App on VM using Cloud Build!"
+    return "🚀 Auto Deployed Flask App on VM using Cloud Build pr!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
